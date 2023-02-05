69916c759d159864c2cdcf4d7064dbeedca74ce5
dev-dhan <roldhan.fulo.alcantara@gmail.com>
2023-02-06 00:09:10 +1100

    adding go up button

--- js/script.js
+++ js/script.js
@@ -54,3 +54,24 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+
+ 
+     /**Go Up Button */
+     $(document).ready(function(){
+
+      $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+          $('#topBtn').fadeIn();
+        } else{
+          $('#topBtn').fadeOut();
+        }
+      });
+    
+      $("#topBtn").click(function(){
+        $('html ,body').animate({scrollTop : 0},800);
+      });
+    });
+      
+      
